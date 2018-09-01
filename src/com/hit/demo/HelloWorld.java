7a6cfbdbdf8b67ea5fb3fc9eb6243a4607cc0a9b
wsx160 <wangshengxian@outlook.com>
2018-09-01 22:12:57 +0800

    ok

---
+++ src/com/hit/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.hit.demo;
+
+public class HelloWorld {
+    public static void main(String[] args)
+
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/com/hit/demo/TestUser.java
@@ -0,0 +1,7 @@
+package com.hit.demo;
+
+/**
+ * Created by ZJGS on 2018/9/1.
+ */
+public class TestUser {
+}
