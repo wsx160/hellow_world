ae10a808bb0f77bfdc8505e84410d884cdfd6118
wsx160 <wangshengxian@outlook.com>
2018-09-02 21:10:28 +0800

    ok

--- src/com/hit/demo/HelloWorld.java
+++ src/com/hit/demo/HelloWorld.java
@@ -1,9 +1,10 @@
 package com.hit.demo;
 
-public class HelloWorld {
-    public static void main(String[] args)
 
-    {
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
         System.out.println("Hello World! ok");
     }
 }
